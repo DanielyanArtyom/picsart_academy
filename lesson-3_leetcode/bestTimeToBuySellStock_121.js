f57eb9810035cd0ee28e7474f9e33c64851e25db
ArtyomMac <>
2023-04-13 20:52:14 +0400

    start lesson 3, commented few of previous codes

---
+++ lesson-3_leetcode/bestTimeToBuySellStock_121.js
@@ -0,0 +1,24 @@
+const _test_1 = [7, 1, 5, 3, 6, 4];
+const _test_2 = [1, 2, 0];
+
+const maxProfit = function (prices) {
+  let currentMax = 0;
+  let idx = 0;
+  let i = 0;
+
+  while (i < prices.length) {
+    if (prices[i] < prices[idx]) {
+      idx = i;
+    }
+
+    const temp = prices[i] - prices[idx];
+
+    currentMax = currentMax < temp ? temp : currentMax;
+
+    ++i;
+  }
+
+  return currentMax;
+};
+
+console.log(maxProfit(_test_2));
---
+++ lesson-3_leetcode/maximumProductsSubarray_152.js
@@ -0,0 +1,18 @@
+const test = [2, 3, -2, 4];
+
+const maxProduct = function (nums) {
+  let currentMax = 1;
+  let currentMin = 1;
+  let result = nums[0]
+
+  if(nums.length === 1) {
+    return nums[0]
+  }
+
+  for (let i = 1; i < nums.length; ++i) {
+    
+}
+
+//   result = max(result,)
+  return result
+};
