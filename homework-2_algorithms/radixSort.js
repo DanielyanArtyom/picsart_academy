d4799cde02b2a5374b6ad3ce0e71d1ab97d11734
ArtyomMac <>
2023-04-23 11:10:32 +0400

    solved radix sort

--- homework-2_algorithms/radixSort.js
+++ homework-2_algorithms/radixSort.js
@@ -1,8 +1,26 @@
-const test1 = [
-  4, 5, 2, 3, 1, 2, 2, 5, 20, 4, 25, 13, -11, -2, 3, -2, 10, 5, 5, 5, -11,
-];
+const test1 = [4, 5, 2, 3, 1, 2, 2, 5, 20, 4, 25, 13, 3, 10, 5, 5, 5];
+
+function getDigit(num, place) {
+  return Math.floor(Math.abs(num) / Math.pow(10, place)) % 10;
+}
 
 const radixSort = (nums) => {
+  let multiplier = 1;
+  let maxDigits = Math.max(...nums);
+
+  while (multiplier < maxDigits) {
+    let positions = Array.from({ length: 10 }, () => []);
+
+    nums.forEach((el) => {
+      let slicedElement = Math.floor(el / multiplier);
+
+      positions[slicedElement % 10].push(el);
+    });
+    
+    nums = [].concat(...positions);
+    ++multiplier;
+  }
+
   return nums;
 };
 
